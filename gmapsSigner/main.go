e8ecc9dea30970a618ed262a9be58205b685a15f
Tyler Kieran Johnson <tyler.kieran.johnson@gmail.com>
2016-01-13 10:34:54 +1100

    change tabs to spaces

--- gmapsSigner/main.go
+++ gmapsSigner/main.go
@@ -1,28 +1,28 @@
 package gmapsSigner
 
 import (
-	"crypto/sha1"
-	"crypto/hmac"
+    "crypto/sha1"
+    "crypto/hmac"
     "encoding/base64"
-	"log"
-	"net/url"
+    "log"
+    "net/url"
 )
 
 func SignUrl(secretKey string, fullUrl string) string {
 
-	u, err := url.Parse(fullUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
+    u, err := url.Parse(fullUrl)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	urlToSign := u.Path + "?" + u.RawQuery
+    urlToSign := u.Path + "?" + u.RawQuery
 
-	key, _ := base64.URLEncoding.DecodeString(secretKey)
-	h := hmac.New(sha1.New, key)
-	h.Write([]byte(urlToSign))
+    key, _ := base64.URLEncoding.DecodeString(secretKey)
+    h := hmac.New(sha1.New, key)
+    h.Write([]byte(urlToSign))
 
 
-	signedUrl := fullUrl + "&signature=" + base64.URLEncoding.EncodeToString(h.Sum(nil))
+    signedUrl := fullUrl + "&signature=" + base64.URLEncoding.EncodeToString(h.Sum(nil))
 
-	return signedUrl
+    return signedUrl
 }
