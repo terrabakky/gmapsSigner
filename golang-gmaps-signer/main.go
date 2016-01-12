a7afdc30a04e81db30bd2dc58a3ef5e163a7b7bf
Tyler Kieran Johnson <tyler.kieran.johnson@gmail.com>
2016-01-13 09:12:39 +1100

    cleaning up

--- golang-gmaps-signer/main.go
+++ golang-gmaps-signer/main.go
@@ -9,7 +9,6 @@ import (
 	"net/url"
 )
 
-
 func SignUrl(secretKey string, fullUrl string) string {
 
 	u, err := url.Parse(fullUrl)
@@ -19,21 +18,19 @@ func SignUrl(secretKey string, fullUrl string) string {
 
 	urlToSign := u.Path + "?" + u.RawQuery
 
-	fmt.Println("URL TO SIGN: " + urlToSign)
-
 	key, _ := base64.URLEncoding.DecodeString(secretKey)
 	h := hmac.New(sha1.New, key)
 	h.Write([]byte(urlToSign))
-
 
 	return base64.URLEncoding.EncodeToString(h.Sum(nil))
 }
 
 func main() {
-	// Sig should be: vBayVIo1sb7_5LJ-uEddsadsL0g=
-
 	secretKey := "chaRF2hTJKOScPr-RQCEhZbSzIE="
 	fullUrl := "http://maps.googleapis.com/maps/api/geocode/json?client=gme-test123"
 
-	fmt.Println("Signature: " + SignUrl(secretKey, fullUrl))
+	fmt.Println("Url to sign: " + fullUrl)
+
+	signedUrl := fullUrl + "&signature=" + SignUrl(secretKey, fullUrl)
+	fmt.Println("Signed Url: " + signedUrl)
 }
