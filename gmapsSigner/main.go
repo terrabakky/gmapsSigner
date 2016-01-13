aad52374255fe3d95bdcb4e09d2ace614e08e59f
Tyler Kieran Johnson <tyler.kieran.johnson@gmail.com>
2016-01-13 11:12:30 +1100

    commenting the package

--- gmapsSigner/main.go
+++ gmapsSigner/main.go
@@ -8,6 +8,8 @@ import (
     "net/url"
 )
 
+// SignUrl takes a secret key and a url to be signed and returns the request
+// url with a signature parameter appended to the end
 func SignUrl(secretKey string, fullUrl string) string {
 
     u, err := url.Parse(fullUrl)
@@ -15,14 +17,18 @@ func SignUrl(secretKey string, fullUrl string) string {
         log.Fatal(err)
     }
 
+    // We only sign the path and query portion of the URL
     urlToSign := u.Path + "?" + u.RawQuery
 
+    // Decode the secret key using the bas64 URL encoding standard
     key, _ := base64.URLEncoding.DecodeString(secretKey)
+
+    // Generate the SHA1-HMAC for the request
     h := hmac.New(sha1.New, key)
     h.Write([]byte(urlToSign))
 
-
+    // Build up the request URL and append the generated request signature
     signedUrl := fullUrl + "&signature=" + base64.URLEncoding.EncodeToString(h.Sum(nil))
-
+    
     return signedUrl
 }
